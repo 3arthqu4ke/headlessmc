8da6762944379a950771a465029ab7486c4f263a
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2022-06-20 16:02:15 +0200

    [1.0.6]: Fixed java regex

--- headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/java/JavaVersionParser.java
+++ headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/java/JavaVersionParser.java
@@ -4,14 +4,13 @@ import lombok.Cleanup;
 import me.earth.headlessmc.launcher.util.IOUtil;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class JavaVersionParser {
     private static final Pattern PATTERN = Pattern.compile(
-        "version \"([0-9]+\\.[0-9]+\\.[0-9_]+)\"");
+        "version \"([0-9]+\\.[0-9]+\\.[0-9]+(?:\\.[0-9]+)*)\"");
 
     public int parse(String path) throws IOException {
         Process prcs = new ProcessBuilder().command(path, "-version").start();
