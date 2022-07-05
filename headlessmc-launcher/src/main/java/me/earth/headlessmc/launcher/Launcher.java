48c59996eb99a7a4e3270cb222fe1022f0718ada
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2022-07-05 14:09:04 +0200

    [1.2.0] Automatically update version number

--- headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/Launcher.java
+++ headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/Launcher.java
@@ -14,7 +14,7 @@ import me.earth.headlessmc.launcher.version.VersionService;
 @Getter
 @RequiredArgsConstructor
 public class Launcher implements HeadlessMc {
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
 
     @Delegate
     private final HeadlessMc headlessMc;
