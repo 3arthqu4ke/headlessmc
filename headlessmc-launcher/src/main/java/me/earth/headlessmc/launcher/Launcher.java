58f20d77d3e0bf528919a82135891b60900566a5
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2022-07-17 13:20:04 +0200

    [1.4.0] Update version

--- headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/Launcher.java
+++ headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/Launcher.java
@@ -15,7 +15,7 @@ import me.earth.headlessmc.launcher.version.VersionService;
 @Getter
 @RequiredArgsConstructor
 public class Launcher implements HeadlessMc {
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.4.0";
 
     @Delegate
     private final HeadlessMc headlessMc;
