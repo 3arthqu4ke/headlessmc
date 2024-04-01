9bd2643d2c439724b7069a436fca852c4c8fe6c6
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-04-02 00:59:35 +0200

    [1.9.3] Bump version

--- headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/Launcher.java
+++ headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/Launcher.java
@@ -15,7 +15,7 @@ import me.earth.headlessmc.launcher.version.VersionService;
 @Getter
 @RequiredArgsConstructor
 public class Launcher implements HeadlessMc {
-    public static final String VERSION = "1.9.2";
+    public static final String VERSION = "1.9.3";
 
     @Delegate
     private final HeadlessMc headlessMc;
