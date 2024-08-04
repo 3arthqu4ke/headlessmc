7cbc1364e7ec668bf8e20cf2afaa444562c7bb62
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-08-04 20:32:07 +0200

    Added some tests

---
+++ headlessmc-lwjgl/src/test/java/me/earth/headlessmc/lwjgl/transformer/AsmUtilTest.java
@@ -0,0 +1,12 @@
+package me.earth.headlessmc.lwjgl.transformer;
+
+import me.earth.headlessmc.util.AbstractUtilityTest;
+import org.junit.jupiter.api.Test;
+
+public class AsmUtilTest extends AbstractUtilityTest<AsmUtil> {
+    @Test
+    public void testRead() {
+        // ...
+    }
+
+}
---
+++ headlessmc-runtime/src/test/java/me/earth/headlessmc/runtime/util/ClassHelperTest.java
@@ -0,0 +1,38 @@
+package me.earth.headlessmc.runtime.util;
+
+import me.earth.headlessmc.api.LogsMessages;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClassHelperTest {
+    @Test
+    @Disabled // test if different on different JVM versions
+    public void testDumpClass() {
+        TestLogsMessages testLogsMessages = new TestLogsMessages();
+        ClassHelper.of(TestLogsMessages.class).dump(testLogsMessages, true);
+        assertEquals("-----------------------------------", testLogsMessages.messages.get(0));
+        assertEquals("me.earth.headlessmc.runtime.util.ClassHelperTest$TestLogsMessages : java.lang.Object, me.earth.headlessmc.api.LogsMessages", testLogsMessages.messages.get(1));
+        assertEquals("--------------Fields---------------", testLogsMessages.messages.get(2));
+
+        ClassHelper.of(Object.class).dump(testLogsMessages, true);
+        // assert
+
+        ClassHelper.of(Object.class).dump(testLogsMessages, false);
+        // assert
+    }
+
+    public static class TestLogsMessages implements LogsMessages {
+        public final List<String> messages = new ArrayList<>();
+
+        @Override
+        public void log(String message) {
+            messages.add(message);
+        }
+    }
+
+}
