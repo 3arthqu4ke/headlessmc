ba19d14852146b289c2e86975c4267b659a47ee9
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-08-04 19:17:45 +0200

    Bumped version to 1.11

--- headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/Launcher.java
+++ headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/Launcher.java
@@ -14,7 +14,7 @@ import me.earth.headlessmc.launcher.version.VersionService;
 @Getter
 @RequiredArgsConstructor
 public class Launcher implements HeadlessMc {
-    public static final String VERSION = "1.10.3";
+    public static final String VERSION = "1.11";
 
     @Delegate
     private final HeadlessMc headlessMc;
