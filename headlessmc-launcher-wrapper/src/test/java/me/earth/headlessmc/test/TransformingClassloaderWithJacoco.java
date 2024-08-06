99430769e84c51c5f50a4ef03b5f7073d3b2b6f4
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-08-06 13:47:05 +0200

    Fixed jacoco coverage, removed test module

--- headlessmc-launcher-wrapper/src/test/java/me/earth/headlessmc/test/TransformingClassloaderWithJacoco.java
+++ headlessmc-launcher-wrapper/src/test/java/me/earth/headlessmc/test/TransformingClassloaderWithJacoco.java
@@ -2,35 +2,20 @@ package me.earth.headlessmc.test;
 
 import me.earth.headlessmc.wrapper.plugin.TransformerPlugin;
 import me.earth.headlessmc.wrapper.plugin.TransformingClassloader;
-import org.jacoco.agent.rt.RT;
-import org.jacoco.core.instr.Instrumenter;
-import org.jacoco.core.runtime.IRuntime;
-import org.jacoco.core.runtime.LoggerRuntime;
-import org.jacoco.core.runtime.RuntimeData;
 
-import java.io.IOException;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.List;
 
 public class TransformingClassloaderWithJacoco extends TransformingClassloader {
-    private final IRuntime runtime = new LoggerRuntime();
-    private final Instrumenter instrumenter = new Instrumenter(runtime);
-
     public TransformingClassloaderWithJacoco(URL[] urls, ClassLoader parent, URLClassLoader transformerClassloader, List<TransformerPlugin> plugins) {
         super(urls, parent, transformerClassloader, plugins);
-        RT.getAgent().get
     }
 
     @Override
     protected byte[] instrument(String name, byte[] classBytes) throws ClassNotFoundException {
         byte[] result = super.instrument(name, classBytes);
-        try {
-            return instrumenter.instrument(result, name);
-        } catch (IOException e) {
-            throw new ClassNotFoundException("Jacoco failed to instrument " + name, e);
-        }
+        return result;
     }
 
 }
