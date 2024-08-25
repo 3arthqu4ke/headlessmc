57461d77c15ad44f302fdf163b64d11444275a11
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-08-25 22:40:31 +0200

    Built-In HeadlessMc-Optimizations support

--- headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/specifics/VersionSpecificMods.java
+++ headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/specifics/VersionSpecificMods.java
@@ -24,7 +24,11 @@ public class VersionSpecificMods {
     public static final VersionSpecificModRepository MC_RUNTIME_TEST =
         new VersionSpecificModRepository(URLs.url("https://github.com/3arthqu4ke/mc-runtime-test/releases/download/"), "mc-runtime-test", "2.4.0", "-release");
 
-    // Not yet... public static final VersionSpecificModRepository HMC_OPTIMIZATIONS =
-    //    new VersionSpecificModRepository(URLs.url("https://github.com/3arthqu4ke/hmc-optimizations/releases/download/"), "hmc-optimizations", "1.0.0", "-release");
+    /**
+     * Version specific optimizations for HeadlessMc. In addition to HeadlessMc's lwjgl patching it patches out all rendering related code.
+     * @see <a href=https://github.com/3arthqu4ke/hmc-optimizations>https://github.com/3arthqu4ke/hmc-optimizations</a>
+     */
+    public static final VersionSpecificModRepository HMC_OPTIMIZATIONS =
+        new VersionSpecificModRepository(URLs.url("https://github.com/3arthqu4ke/hmc-optimizations/releases/download/"), "hmc-optimizations", "0.4.0", "");
 
 }
