a7e299ab5caacd5fc0f76dc5e8ce9ba82bb0fa52
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2024-09-01 13:06:16 +0200

    2.3.0

--- headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/Launcher.java
+++ headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/Launcher.java
@@ -28,7 +28,7 @@ public class Launcher implements HeadlessMc {
     /**
      * The current version of HeadlessMc.
      */
-    public static final String VERSION = "2.2.0";
+    public static final String VERSION = "2.3.0";
 
     /**
      * A HeadlessMc instance that this Launcher delegates to for CommandLine, ExitManager, etc..
