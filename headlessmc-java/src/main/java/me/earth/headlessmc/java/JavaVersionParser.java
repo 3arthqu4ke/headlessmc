39883aa254745b73508a27b8e6d39af5a46acb4a
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2025-01-28 21:11:28 +0100

    Add execute permissions to jspawnhelper

--- headlessmc-java/src/main/java/me/earth/headlessmc/java/JavaVersionParser.java
+++ headlessmc-java/src/main/java/me/earth/headlessmc/java/JavaVersionParser.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermission;
 import java.util.HashSet;
@@ -25,21 +26,27 @@ public class JavaVersionParser {
 
     public int parseVersionCommand(String path) throws IOException {
         if (addFilePermissions) {
+            Path executable = Paths.get(path);
+            // we should probably take the permissions of the file and just add execute instead
             Set<PosixFilePermission> executePermissions = new HashSet<>();
             executePermissions.add(PosixFilePermission.OWNER_READ);
-            //executePermissions.add(PosixFilePermission.OWNER_WRITE);
             executePermissions.add(PosixFilePermission.OWNER_EXECUTE);
             executePermissions.add(PosixFilePermission.GROUP_READ);
-            //executePermissions.add(PosixFilePermission.GROUP_WRITE);
             executePermissions.add(PosixFilePermission.GROUP_EXECUTE);
             executePermissions.add(PosixFilePermission.OTHERS_READ);
-            //executePermissions.add(PosixFilePermission.OTHERS_WRITE);
-            executePermissions.add(PosixFilePermission.OTHERS_EXECUTE);
-            Files.setPosixFilePermissions(Paths.get(path), executePermissions);
+            executePermissions.add(PosixFilePermission.OTHERS_EXECUTE); // ?
+            Files.setPosixFilePermissions(executable, executePermissions);
+
+            Path bin = executable.getParent();
+            Path java = bin.getParent();
+            Path jspawnHelper = java.resolve("lib").resolve("jspawnhelper");
+            if (Files.exists(jspawnHelper)) {
+                // https://askubuntu.com/a/1492514
+                Files.setPosixFilePermissions(jspawnHelper, executePermissions);
+            }
         }
 
         Process prcs = new ProcessBuilder().command(path, "-version").start();
-
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(prcs.getErrorStream()))) {
             StringBuilder builder = new StringBuilder();
             String line;
