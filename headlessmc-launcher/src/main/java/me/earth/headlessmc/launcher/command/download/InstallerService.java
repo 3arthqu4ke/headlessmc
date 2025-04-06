0f6f8c4eace3a9d4366447e960d0d7b36c615afc
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2025-04-06 21:15:16 +1000

    Fixed e.g. neoforge:1.21.5 downloading lexforge instead of neo

--- headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/command/download/InstallerService.java
+++ headlessmc-launcher/src/main/java/me/earth/headlessmc/launcher/command/download/InstallerService.java
@@ -13,6 +13,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.EnumMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class InstallerService {
     private final Map<Modlauncher, ModLauncherCommand> modLauncherCommands = new EnumMap<>(Modlauncher.class);
@@ -24,8 +25,10 @@ public class InstallerService {
         // TODO: this is super bad!!!
         this.downloadCommand = findCommand(launcher, DownloadCommand.class, DownloadCommand::new);
         this.modLauncherCommands.put(Modlauncher.FABRIC, findCommand(launcher, FabricCommand.class, FabricCommand::new));
-        this.modLauncherCommands.put(Modlauncher.LEXFORGE, findCommand(launcher, ForgeCommand.class, ForgeCommand::lexforge));
-        this.modLauncherCommands.put(Modlauncher.NEOFORGE, findCommand(launcher, ForgeCommand.class, ForgeCommand::neoforge));
+        this.modLauncherCommands.put(Modlauncher.LEXFORGE, findCommand(launcher, ForgeCommand.class, ForgeCommand::lexforge,
+                c -> "lexforge".equalsIgnoreCase(c.getName()) || "forge".equalsIgnoreCase(c.getName())));
+        this.modLauncherCommands.put(Modlauncher.NEOFORGE, findCommand(launcher, ForgeCommand.class, ForgeCommand::neoforge,
+                c -> "neoforge".equalsIgnoreCase(c.getName())));
     }
 
     public void install(VersionArgument versionArgument, String... args) throws CommandException {
@@ -56,8 +59,16 @@ public class InstallerService {
     }
 
     private <C extends Command> @Nullable C findCommand(Launcher launcher, Class<C> clazz, Function<Launcher, C> fallback) {
+        return findCommand(launcher, clazz, fallback, null);
+    }
+
+    private <C extends Command> @Nullable C findCommand(Launcher launcher, Class<C> clazz, Function<Launcher, C> fallback, @Nullable Predicate<C> predicate) {
         for (Command command : launcher.getCommandLine().getCommandContext()) {
             if (clazz.isAssignableFrom(command.getClass())) {
+                if (predicate != null && !predicate.test((clazz.cast(command)))) {
+                    continue;
+                }
+
                 return clazz.cast(command);
             }
         }
