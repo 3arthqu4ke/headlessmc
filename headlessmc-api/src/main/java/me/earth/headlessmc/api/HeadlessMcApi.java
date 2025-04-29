ac5c90d5b9cd4518aca66afafbfdffc0eb060b9d
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2025-04-29 10:29:21 +1000

    2.6.0

--- headlessmc-api/src/main/java/me/earth/headlessmc/api/HeadlessMcApi.java
+++ headlessmc-api/src/main/java/me/earth/headlessmc/api/HeadlessMcApi.java
@@ -25,7 +25,7 @@ public class HeadlessMcApi {
     /**
      * The current version of HeadlessMc.
      */
-    public static final String VERSION = "2.5.1";
+    public static final String VERSION = "2.6.0";
     /**
      * The string "HeadlessMC".
      */
