fdacd5306c01fc9f0205d51673124d94f9e9d5cf
3arthqu4ke <56741599+3arthqu4ke@users.noreply.github.com>
2025-05-14 12:58:46 +1000

    Configure workflow to publish documentation

--- headlessmc-web/plugin/src/main/java/me/earth/headlessmc/web/cheerpj/plugin/CheerpJLauncher.java
+++ headlessmc-web/plugin/src/main/java/me/earth/headlessmc/web/cheerpj/plugin/CheerpJLauncher.java
@@ -84,7 +84,7 @@ public class CheerpJLauncher {
         Logger logger = LoggerFactory.getLogger("HeadlessMc");
         logger.info("Initializing HeadlessMc...");
         logger.info("This is just a demo!");
-        logger.info("You can find the javadocs at https://3arthqu4ke.github.io/headlessmc/javadoc");
+        logger.info("You can find the documentation at https://3arthqu4ke.github.io/headlessmc/docs");
         logger.info("Powered by CheerpJ!");
         logger.info("https://cheerpj.com/");
         logger.info("CheerpJ currently does not support some features we need, so you will probably not be able to launch the game yet!");
